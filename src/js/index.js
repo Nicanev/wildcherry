717c6ad538313c4fcd30d1e1eb5e5e6910360656
Nick <nic7ven@gmail.com>
2022-09-05 16:05:38 +0400

    fix

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 globalThis.__VUE_OPTIONS_API__ = true;
 globalThis.__VUE_PROD_DEVTOOLS__ = false;
-import { createApp } from 'vue/dist/vue.esm-bundler';
+import { createApp } from 'vue/dist/vue.esm-bundler.js';
 import App from "./App.vue";
 
 const app = createApp(App);
