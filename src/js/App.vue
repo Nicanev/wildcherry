86de7ca6fddbe31c8997d44585b42522ceefbde5
Nick <nic7ven@gmail.com>
2022-09-05 15:38:42 +0400

    add open catalog

---
+++ src/js/App.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  data() {
+    return {
+      isCatalogOpen: false,
+      currentCatalogCategory: "Electronic",
+    };
+  },
+  methods: {
+    openCatalog() {
+      this.isCatalogOpen = !this.isCatalogOpen;
+    },
+    selectCatalogCategory(category) {
+      this.currentCatalogCategory = category;
+    },
+  },
+};
+</script>
---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+globalThis.__VUE_OPTIONS_API__ = true;
+globalThis.__VUE_PROD_DEVTOOLS__ = false;
+import { createApp } from 'vue/dist/vue.esm-bundler';
+import App from "./App.vue";
+
+const app = createApp(App);
+app.mount("#app");
